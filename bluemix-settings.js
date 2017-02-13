cbdd2c0b2c0c6754c32ab961247c6cc6e04869fe
Asheni De Silva <asheni_silva@accelerite.com>
2017-02-13 05:57:50 -0600

    Redis integration

--- bluemix-settings.js
+++ bluemix-settings.js
@@ -27,7 +27,7 @@ var settings = module.exports = {
     uiPort: process.env.PORT || 1880,
     mqttReconnectTime: 15000,
     serialReconnectTime: 15000,
-    debugMaxLength: 1000,
+    debugMaxLength: 5000,
 
     // Add the bluemix-specific nodes in
     nodesDir: path.join(__dirname,"nodes"),
